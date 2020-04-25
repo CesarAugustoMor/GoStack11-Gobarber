a0018233f1efcd05e291fb5fca32bc8fd57d49a5
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-04-25 11:28:49 -0300

    Criação de repositorios

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,24 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+export default class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    return (
+      this.appointments.find(appointment => isEqual(date, appointment.date)) ||
+      null
+    );
+  }
+}
