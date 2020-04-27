07d535619ee760b04a9f50ccaf1f2177d547ea03
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-04-27 10:10:45 -0300

    Relacionamento dos modelos

--- src/database/migrations/1587989784661-CreateUsers.ts
+++ src/database/migrations/1587989784661-CreateUsers.ts
@@ -9,7 +9,7 @@ export default class CreateUsers1587989784661 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
