fd37e0cb3b75926a8ba63c0b860a3e3508956c3c
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-05 13:02:37 -0300

    Utilizando cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import 'express-async-errors';
 
 import AppError from './errors/AppError';
@@ -8,6 +9,8 @@ import routes from './routes';
 import uploadConfig from './config/upload';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
