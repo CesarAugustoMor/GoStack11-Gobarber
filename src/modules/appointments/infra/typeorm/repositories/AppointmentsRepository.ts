edf3c70e6362ed3c3659acdb83a2bd9028ae1f31
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-13 11:24:15 -0300

    Reescrevendo Repositórios

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,17 +1,39 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
-@EntityRepository(Appointment)
-export default class AppointmentsRepository extends Repository<Appointment>
-  implements IAppointmentsRepository {
+export default class AppointmentsRepository implements IAppointmentsRepository {
+  private ormRepository: Repository<Appointment>;
+
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   /**
    * Procura se a data já está cadastrada.
    * @param date Data a ser procurada
    */
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    return this.findOne({ where: { date } });
+    return this.ormRepository.findOne({ where: { date } });
+  }
+
+  /**
+   * create
+   */
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({
+      provider_id,
+      date,
+    });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
   }
 }
