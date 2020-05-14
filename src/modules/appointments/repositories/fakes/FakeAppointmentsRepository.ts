a4ed3d73efec21663ae709b08e5603e46ad37bf0
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-14 10:44:53 -0300

    Configuração de testes e teste Create Appointment

---
+++ src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,41 @@
+import { uuid } from 'uuidv4';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
+
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+
+export default class AppointmentsRepository implements IAppointmentsRepository {
+  private appointments: Appointment[] = [];
+
+  /**
+   * Procura se a data já está cadastrada.
+   * @param date Data a ser procurada
+   */
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const findAppointment = this.appointments.find(
+      appointment => appointment.date === date,
+    );
+    if (!findAppointment) {
+      return undefined;
+    }
+
+    return findAppointment;
+  }
+
+  /**
+   * create
+   */
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment();
+
+    Object.assign(appointment, { id: uuid(), date, provider_id });
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
