7db91ee46669822d8d57caa4a183fdd43ee39228
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-19 11:59:04 -0300

    Agenda do prestador

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,43 @@
+import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let fakeAppointmentsRepository: FakeAppointmentsRepository;
+let listProviderAppointments: ListProviderAppointmentsService;
+
+describe('ListProviderAppointments', () => {
+  beforeEach(() => {
+    fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    listProviderAppointments = new ListProviderAppointmentsService(
+      fakeAppointmentsRepository,
+    );
+  });
+
+  it('shuld be able to list the appointments on a specific day', async () => {
+    const appointment1 = await fakeAppointmentsRepository.create({
+      user_id: 'user',
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 8, 0, 0),
+    });
+
+    const appointment2 = await fakeAppointmentsRepository.create({
+      user_id: 'user',
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 12, 0, 0),
+    });
+
+    const appointment3 = await fakeAppointmentsRepository.create({
+      user_id: 'user',
+      provider_id: 'provider',
+      date: new Date(2020, 4, 20, 15, 0, 0),
+    });
+
+    const Appointments = await listProviderAppointments.execute({
+      provider_id: 'provider',
+      month: 5,
+      year: 2020,
+      day: 20,
+    });
+
+    expect(Appointments).toEqual([appointment1, appointment2, appointment3]);
+  });
+});
