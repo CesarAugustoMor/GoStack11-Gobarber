7daa6018004cd536658fa73a26fc38f4e32f8e21
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-20 14:16:48 -0300

    Upload de arquivos para o Amazon S3

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -6,6 +6,7 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 import { Exclude, Expose } from 'class-transformer';
+import uploadConfig from '@config/upload';
 
 @Entity('users')
 export default class User {
@@ -33,8 +34,18 @@ export default class User {
 
   @Expose({ name: 'avatar_url' })
   getavatar_url(): string | null {
-    return this.avatar
-      ? `${process.env.APP_API_URL}/files/${this.avatar}`
-      : null;
+    if (!this.avatar) {
+      return null;
+    }
+
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+
+      case 's3':
+        return `http://${uploadConfig.config.aws.buket}.s3.amazonaws.com/${this.avatar}`;
+      default:
+        return null;
+    }
   }
 }
