c687b9396c2e5971e4ac48a9690be9eeb5d8a927
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-20 10:18:19 -0300

    Validando dados

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ForgotPasswordController from '@modules/users/infra/http/controllers/ForgotPasswordController';
 import ResetForgotPasswordController from '@modules/users/infra/http/controllers/ResetForgotPasswordController';
@@ -7,7 +8,25 @@ const passwordRouter = Router();
 const forgotPasswordController = new ForgotPasswordController();
 const restForgotPasswordController = new ResetForgotPasswordController();
 
-passwordRouter.post('/forgot', forgotPasswordController.create);
-passwordRouter.post('/reset', restForgotPasswordController.create);
+passwordRouter.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
+passwordRouter.post(
+  '/reset',
+  celebrate({
+    [Segments.BODY]: {
+      token: Joi.string().required().uuid(),
+      password: Joi.string().required(),
+      password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+    },
+  }),
+  restForgotPasswordController.create,
+);
 
 export default passwordRouter;
--- src/modules/users/infra/http/routes/userProfile.routes.ts
+++ src/modules/users/infra/http/routes/userProfile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
@@ -10,6 +11,18 @@ const usersProfileController = new UserProfileController();
 usersProfileRouter.use(ensureAuthenticated);
 
 usersProfileRouter.get('/', usersProfileController.show);
-usersProfileRouter.put('/', usersProfileController.update);
+usersProfileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      old_password: Joi.string(),
+      password: Joi.string(),
+      password_confiramtion: Joi.string().valid(Joi.ref('password')),
+    },
+  }),
+  usersProfileController.update,
+);
 
 export default usersProfileRouter;
