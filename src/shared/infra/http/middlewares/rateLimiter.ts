cb76507bfeab673cd6c0e8d5b64dbd06467b157d
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-20 17:45:51 -0300

    Express rate limit

---
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -0,0 +1,32 @@
+import { Response, Request, NextFunction } from 'express';
+import { RateLimiterRedis } from 'rate-limiter-flexible';
+import redis from 'redis';
+
+import AppError from '@shared/errors/AppError';
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASS || undefined,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: 'rateLimits',
+  points: 5,
+  duration: 1,
+});
+
+export default async function reteLimiter(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(req.ip);
+
+    return next();
+  } catch (error) {
+    throw new AppError('To many requests', 429);
+  }
+}
