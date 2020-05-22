d3f42139c89893615b7a2fd8047245b409589d0f
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-22 10:31:22 -0300

    Dias indisponíveis no mês

--- src/modules/appointments/services/ListProviderMonthAvailabiliryService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabiliryService.ts
@@ -1,7 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
-import { getDaysInMonth, getDate } from 'date-fns';
+import { getDaysInMonth, getDate, isAfter } from 'date-fns';
 
 interface IRequest {
   provider_id: string;
@@ -48,9 +48,11 @@ export default class ListProviderMonthAvailabiliryService {
         return getDate(appointment.date) === day;
       });
 
+      const compareDate = new Date(year, month - 1, day, 23, 59, 59);
       return {
         day,
-        available: appointmentsInDay.length < 10,
+        available:
+          isAfter(compareDate, new Date()) && appointmentsInDay.length < 10,
       };
     });
 
