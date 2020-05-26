159fd1b35f1fd484ac1a9b6ee202a80121f97469
César Augusto Moro Fürst <cesaramfurst@gmail.com>
2020-05-26 15:49:58 -0300

    Removendo limite de acesso para arquivos estáticos

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,20 +1,14 @@
-import { Router } from 'express';
-import { celebrate, Segments, Joi } from 'celebrate';
-
-import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
 import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import { celebrate, Joi, Segments } from 'celebrate';
+import { Router } from 'express';
 
 const appointmentsRouter = Router();
 const appointmentsController = new AppointmentsController();
 const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
-
-// appointmentsRouter.get('/', async (req, res) => {
-// const appointmentsRepository = new AppointmentsRepository();
-//   return res.json(await appointmentsRepository.find());
-// });
 
 appointmentsRouter.post(
   '/',
